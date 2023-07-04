cac87ec0dbc9cb7b504cb376491df42532fd8d70
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-04 06:00:29 -0400

    update home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -22,75 +22,137 @@ function Home() {
     color: "",
     dkkd: "",
     bct_link: "",
-    showSeatId: -1
+    showSeatId: 0
   });
 
+  const tickBox = ['showSeatId'];
+
   const fetchData = async () => {
-    await axios.get('http://localhost:5000/home/1877').then(res => console.log(res.data));
+    try {
+      const res = await axios.get('http://localhost:5000/home/1877');
+      setData({ ...res.data, address: JSON.parse(res.data.address) });
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
 
   useEffect(() => {
     fetchData();
   }, [])
 
-  const onFinish = (values) => {
-    console.log('Form values:', values);
+  const onFinish = () => {
+    console.log('Form values:', data);
   };
+
+  const handleChange = (e, type, index) => {
+    //tickbox handle
+    if (tickBox.includes(type)) {
+      const value = e.target.checked;
+      let num = value ? 1 : 0;
+      setData(prev => ({ ...prev, [type]: num }));
+    }
+    //input field handle
+    else {
+      const value = e.currentTarget.value;
+
+      //if handle address
+      if (index >= 0) {
+        let addresses = [...data.address];
+        addresses[index][type] = value;
+        setData(prev => ({ ...prev, address: addresses }));
+      }
+      //handle other
+      else {
+        setData(prev => ({ ...prev, [type]: value }));
+      }
+    }
+  }
+
+  const refreshPage = () => {
+    window.location.reload();
+    window.scrollTo(0, 0);
+  }
+
+  const deleteAddress = (index) => {
+    console.log("Deleting address: " + index);
+  }
 
   return (
     <Form onFinish={onFinish}>
       <div className='form'>
         <div className='inner-form'>
-          <Form.Item label="Tên công ty" name="business" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Tên công ty" initialValue="">
+            <Input className='form-input' value={data.business} onChange={(e) => handleChange(e, 'business')} />
           </Form.Item>
 
-          <Form.Item label="Giấy chứng nhận DKKD" name="dkkd" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Giấy chứng nhận DKKD" initialValue="">
+            <Input className='form-input' value={data.dkkd} onChange={(e) => handleChange(e, 'dkkd')} />
           </Form.Item>
-          <Form.Item label="Đường dẫn DK BCT" name="bct_link" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Đường dẫn DK BCT" initialValue="">
+            <Input className='form-input' value={data.bct_link} onChange={(e) => handleChange(e, 'bct_link')} />
           </Form.Item>
-          <Form.Item label="Giới thiệu chung" name="short_intro" initialValue="">
-            <Input className='form-input' />
-          </Form.Item>
-
-          <Form.Item label="Số điện thoại" name="phone" initialValue="">
-            <Input className='form-input' />
-          </Form.Item>
-          <Form.Item label="Hotline" name="hotline" initialValue="">
-            <Input className='form-input' />
-          </Form.Item>
-          <Form.Item label="Email" name="email" initialValue="">
-            <Input className='form-input' />
-          </Form.Item>
-          <Form.Item label="Map" name="map" initialValue="">
-            <Input.TextArea className='form-input' rows={4} />
+          <Form.Item label="Giới thiệu chung" initialValue="">
+            <Input className='form-input' value={data.short_intro} onChange={(e) => handleChange(e, 'short_intro')} />
           </Form.Item>
 
+          <Form.Item label="Số điện thoại" initialValue="">
+            <Input className='form-input' value={data.phone} onChange={(e) => handleChange(e, 'phone')} />
+          </Form.Item>
+          <Form.Item label="Hotline" initialValue="">
+            <Input className='form-input' value={data.hotline} onChange={(e) => handleChange(e, 'hotline')} />
+          </Form.Item>
+          <Form.Item label="Email" initialValue="">
+            <Input className='form-input' value={data.email} onChange={(e) => handleChange(e, 'email')} />
+          </Form.Item>
+          <Form.Item label="Map" initialValue="">
+            <Input.TextArea className='form-input' rows={8} value={data.map} onChange={(e) => handleChange(e, 'map')} />
+          </Form.Item>
 
-          <Form.Item label="Hiển thị mã ghế" name="showSeatId" valuePropName="checked">
-            <Checkbox />
+          <Form.Item label="Địa chỉ">
+            {
+              data.address.map((item, index) => {
+                return (
+                  <div className='form-address-container' key={index}>
+                    <Input className='form-input' value={item.key} onChange={(e) => handleChange(e, 'key', index)} />
+                    <Input className='form-input' value={item.value} onChange={(e) => handleChange(e, 'value', index)} />
+                    <Button type='primary' danger onClick={() => deleteAddress(index)}>-</Button>
+                  </div>
+                )
+              })
+            }
+          </Form.Item>
+
+
+          <Form.Item label="Hiển thị mã ghế" valuePropName="checked">
+            <Checkbox checked={data.showSeatId === 1} onChange={(e) => handleChange(e, 'showSeatId')} />
           </Form.Item>
         </div>
 
         <div className='inner-form'>
-          <Form.Item label="Meta Title" name="meta_title" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Meta Title" initialValue="">
+            <Input className='form-input' value={data.meta_title} onChange={(e) => handleChange(e, 'meta_title')} />
           </Form.Item>
-          <Form.Item label="Meta Keyword" name="meta_keyword" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Meta Keyword" initialValue="">
+            <Input className='form-input' value={data.meta_keyword} onChange={(e) => handleChange(e, 'meta_keyword')} />
           </Form.Item>
-          <Form.Item label="Meta Description" name="meta_description" initialValue="">
-            <Input className='form-input' />
+          <Form.Item label="Meta Description" initialValue="">
+            <Input className='form-input' value={data.meta_description} onChange={(e) => handleChange(e, 'meta_description')} />
           </Form.Item>
         </div>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className='form-button'>
-            Submit
-          </Button>
-        </Form.Item>
       </div>
+      <div className='form-submit-buttons'>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className='form-button'>
+              Lưu
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" className='form-button' onClick={() => refreshPage()}>
+              Huỷ
+            </Button>
+          </Form.Item>
+        </div>
     </Form>
   )
 }
