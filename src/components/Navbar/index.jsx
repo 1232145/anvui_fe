acc5173305ad5cf5c8facea6358aced995a01f75
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-07 01:45:27 -0400

    add nav menu and routes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import { AppstoreOutlined, MailOutlined, PieChartOutlined, DesktopOutlined, ContainerOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-import { publicRoutes } from '../../routes';
+import { navRoutes } from '../../routes';
 import { useNavigate } from 'react-router-dom';
 import './nav.css';
 
 //key == path
 const getItem = (label, key, icon, children) => {
-    return {key, icon, children, label};
+    return { key, icon, children, label };
 }
-const items = [
-    getItem('Trang chủ', '/', <PieChartOutlined />),
-    getItem('Page', '/page', <DesktopOutlined />),
-    getItem('Option 3', '3', <ContainerOutlined />),
-    getItem('Tin tức', 'sub1', <MailOutlined />, [
-        getItem('Option 5', '5'),
-        getItem('Option 6', '6'),
-        getItem('Option 7', '7'),
-        getItem('Option 8', '8'),
-    ]),
-    getItem('Cài đặt', 'sub2', <AppstoreOutlined />, [
-        getItem('Option 9', '9'),
-        getItem('Option 10', '10'),
-        getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
-    ]),
-];
+
+const items = navRoutes.map(item => {
+    return getItem(item.name, item.path, item.icon, item.children?.map(item => {
+        return getItem(item.name, item.path);
+    }));
+});
 
 const Nav = () => {
     const navigate = useNavigate();
 
     return (
         <Menu
-            onClick={({key}) => navigate(key)}
+            onClick={({ key }) => navigate(key)}
             defaultSelectedKeys={['1']}
             defaultOpenKeys={['sub1']}
             mode="inline"
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,49 @@
 import Home from "../pages/Home";
-import { UserOutlined} from '@ant-design/icons';
-import News from '../pages/News';
+import { UserOutlined } from '@ant-design/icons';
 import Page from '../pages/Page'
-import Setting from "../pages/Setting";
 
 export const publicRoutes = [
-    {path: '/', component: Home, name: "Home", icon: <UserOutlined />},
-    {path: '/news', component: News, name: "News", icon: null, children: [],},
-    {path: '/page', component: Page, name: "Page", icon: null},
-    {path: '/setting', component: Setting, name: "Seting", icon: null},
+    { path: '/', name: "Trang chủ", icon: <UserOutlined />, component: Home },
+    { path: '/news/create-news', name: "Thêm tin tức", component: null },
+    { path: '/news/news-list', name: "Danh sách tin tức", component: null },
+    { path: '/news/create-news-catagory', name: "Thêm danh mục tin tức", component: null },
+    { path: '/news/news-catagory', name: "Danh mục tin tức", component: null },
+    { path: '/page', name: "Page", icon: null, component: Page },
+    { path: '/menu/menu-list', name: "Danh sách menu", component: null },
+    { path: '/template/slide-banner', name: "Slide ảnh và Banner", component: null },
+    { path: '/template/css-custom', name: "Thay đổi CSS", component: null },
+    { path: '/template/change-theme', name: "Thay đổi Theme", component: null },
+    { path: '/setting/payment', name: "Thông tin thanh toán", component: null },
+    { path: '/setting/domain', name: "Tên miền", component: null },
+]
+
+export const navRoutes = [
+    { path: '/', component: Home, name: "Trang chủ", icon: <UserOutlined /> },
+    {
+        path: null, name: "Tin Tức", icon: null, children: [
+            { path: '/news/create-news', name: "Thêm tin tức", component: null },
+            { path: '/news/news-list', name: "Danh sách tin tức", component: null },
+            { path: '/news/create-news-catagory', name: "Thêm danh mục tin tức", component: null },
+            { path: '/news/news-catagory', name: "Danh mục tin tức", component: null },
+        ]
+    },
+    { path: '/page', component: Page, name: "Page", icon: null },
+    {
+        path: 'null', name: "Menu", icon: null, children: [
+            { path: '/menu/menu-list', name: "Danh sách menu", component: null },
+        ]
+    },
+    {
+        path: null, name: "Giao diện", icon: null, children: [
+            { path: '/template/slide-banner', name: "Slide ảnh và Banner", component: null },
+            { path: '/template/css-custom', name: "Thay đổi CSS", component: null },
+            { path: '/template/change-theme', name: "Thay đổi Theme", component: null },
+        ]
+    },
+    {
+        path: '/setting', name: "Seting", icon: null, children: [
+            { path: '/setting/payment', name: "Thông tin thanh toán", component: null },
+            { path: '/setting/domain', name: "Tên miền", component: null },
+        ]
+    },
 ]
