3a7754e8956dcc5cfcadca62f075549e34e476b1
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-12 22:20:01 +0700

    Update api.jsx

--- src/components/Api/api.jsx
+++ src/components/Api/api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'https://anvui-be.vercel.app/';
+const url = 'https://anvui-be.vercel.app';
 const testUrl = 'http://localhost:5000'
 
 const api = axios.create({
