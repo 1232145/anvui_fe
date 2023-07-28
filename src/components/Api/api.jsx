abd1dcd54ff165f6757e5b37f249808882328890
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-28 09:29:20 -0400

    change api url

--- src/components/Api/api.jsx
+++ src/components/Api/api.jsx
@@ -3,7 +3,7 @@ const url = 'https://anvui-be.vercel.app';
 const testUrl = 'http://localhost:5000'
 
 const api = axios.create({
-  baseURL: testUrl, 
+  baseURL: url, 
   headers: {
     'Content-Type': 'application/json',
   },
