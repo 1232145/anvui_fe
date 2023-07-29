7f3cfc3b149f9ddde818f0d0a76da2a6d0701224
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-29 09:47:53 -0400

    update login handle error logic

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -9,7 +9,7 @@ import ErrorLogin from '../../pages/Error/ErrorLoginPage';
 const { Title } = Typography;
 const url = '/login';
 
-const Login = ({setLogin}) => {
+const Login = ({ setLogin }) => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -20,14 +20,27 @@ const Login = ({setLogin}) => {
       setLoading(true);
       const res = await api.post(url, values);
       const data = res.data;
-      localStorage.setItem('accessToken', data.access_token);
-      setLogin(true);
-      navigate('/');
+
+      if (!data.access_token) {
+        setError(true);
+        setErrMsg("Không thể đăng nhập.");
+      }
+      else {
+        localStorage.setItem('accessToken', data.access_token);
+        setLogin(true);
+        navigate('/');
+      }
     }
-    catch(err) {
-      const errorMsg = err.response.data.error;
+    catch (err) {
+      if (err.response) {
+        const errorMsg = err.response.data.error;
+        setErrMsg(errorMsg);
+      }
+      else {
+        setErrMsg("Không thể đăng nhập.");
+      }
+
       setError(true);
-      setErrMsg(errorMsg);
     }
     finally {
       setLoading(false);
@@ -42,84 +55,83 @@ const Login = ({setLogin}) => {
   if (error) {
     return (
       <>
-        <ErrorLogin msg={errMsg} setError={setError}/>
+        <ErrorLogin msg={errMsg} setError={setError} />
       </>
     )
   }
 
   return (
     <>
-    {
-      loading ? 
-      <><Loading /></>
-      :
-      <div>
-        <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#f0f2f5',
-      }}
-    >
-      <div
-        style={{
-          maxWidth: 400,
-          width: '100%',
-          padding: 24,
-          borderRadius: 4,
-          boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
-          backgroundColor: '#fff',
-        }}
-      >
-        <Title level={3} style={{ textAlign: 'center' }}>
-          Đăng Nhập
-        </Title>
-        <Form
-          name="loginForm"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            name="username"
-            rules={[
-              { required: true, message: 'Please enter your username!' },
-            ]}
-          >
-            <Input
-              prefix={<UserOutlined />}
-              placeholder="Tên đăng nhập"
-              size="large"
-            />
-          </Form.Item>
+      {
+        loading ?
+          <><Loading /></>
+          :
+          <div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundColor: '#f0f2f5',
+              }}
+            >
+              <div
+                style={{
+                  maxWidth: 400,
+                  width: '100%',
+                  padding: 24,
+                  borderRadius: 4,
+                  boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+                  backgroundColor: '#fff',
+                }}
+              >
+                <Title level={3} style={{ textAlign: 'center' }}>
+                  Đăng Nhập
+                </Title>
+                <Form
+                  name="loginForm"
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                >
+                  <Form.Item
+                    name="username"
+                    rules={[
+                      { required: true, message: 'Please enter your username!' },
+                    ]}
+                  >
+                    <Input
+                      prefix={<UserOutlined />}
+                      placeholder="Tên đăng nhập"
+                      size="large"
+                    />
+                  </Form.Item>
 
-          <Form.Item
-            name="password"
-            rules={[
-              { required: true, message: 'Please enter your password!' },
-            ]}
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              placeholder="Mật khẩu"
-              size="large"
-            />
-          </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      { required: true, message: 'Please enter your password!' },
+                    ]}
+                  >
+                    <Input.Password
+                      prefix={<LockOutlined />}
+                      placeholder="Mật khẩu"
+                      size="large"
+                    />
+                  </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block size="large">
-              Log in
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>
-      </div>
-    }
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" block size="large">
+                      Log in
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
+            </div>
+          </div>
+      }
     </>
   );
 };
 
 export default Login;
-  
