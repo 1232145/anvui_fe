7a7930b7f507c2e98734f0af9f68dc285058e8db
Duc Huy <51442563+1232145@users.noreply.github.com>
2023-07-30 05:41:59 -0400

    adjust file handle

--- src/pages/Template/Slide.jsx
+++ src/pages/Template/Slide.jsx
@@ -117,15 +117,18 @@ const Slide = () => {
       multiple: true,
       showUploadList: true,
       customRequest: async ({ onSuccess, onError, file }) => {
+
         const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+
         if (!isJpgOrPng) {
           message.error('You can only upload JPG/PNG images!');
-          return false;
+          return;
         }
         const isLt2M = file.size / 1024 / 1024 < 2;
+        
         if (!isLt2M) {
           message.error('Image must be smaller than 2MB!');
-          return false;
+          return;
         }
 
         try {
@@ -143,7 +146,7 @@ const Slide = () => {
           });
         }
         catch (err) {
-          onError("Upload failed")
+          message.error("Upload failed");
         }
         finally {
           fetchData();
